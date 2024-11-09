416b10c619ccf2ffe20bde564d7646db97186d50
Hugo Santos <gabrielhugo438@gmail.com>
2024-11-09 18:36:20 -0300

    feat: Inclusão de módulo de usuários

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { TaskModule } from './task/task.module';
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), TaskModule],
+  imports: [ConfigModule.forRoot(), TaskModule, UsersModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
